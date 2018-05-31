d26890aae4561f64f3b32e6b985a8079ebf76ef7
Jon <jedijon567@gmail.com>
2018-05-30 23:33:06 -0400

    Remove twitter

--- config.py
+++ config.py
@@ -1,13 +1,6 @@
 # Floatzel bot config.py file
 # copyright Ezioisawesome56 2017-2018
 # bot configuration file
-import twitter
-
-# twitter api stuff
-api = twitter.Api(consumer_key='KEY',
-  consumer_secret='KEY',
-  access_token_key='KEY',
-  access_token_secret='KEY')
 
 # bot command prefixes
 fix = "&"
@@ -17,18 +10,8 @@ adminfix = "&%"
 # you get it by turning on dev mode in discord, right clicking your name, and click copy id
 owner = ownerid
 
-# twitter @s to pull tweets from
-twat = "h"
-twattwo = "gay"
-
-# twitter embed picture
-twatcon = """https://cdn2.iconfinder.com/data/icons/minimalism/512/twitter.png"""
-
-# the channel id to post the tweets to
-tweetchan = channelid
-
 # discord bot token
 token = "token"
 
 #bot version
-ver = "1.0.1"
+ver = "1.0.1 Twitter Removal Patch"
